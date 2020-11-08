49e676fabed2feead3a45f2b7afc5cbe2d2a8021
Surace Lucas <surace.lucas2019@gmail.com>
2020-11-08 14:20:05 -0300

    Se agrega Administrador y Consulta, se edita Tienda

---
+++ src/ar/edu/unlam/pb2/eva2/Consulta.java
@@ -0,0 +1,24 @@
+package ar.edu.unlam.pb2.eva2;
+
+public class Consulta {
+	private String nombreUsuario;
+	private String descripcion;
+	
+	public Consulta(String nombreUsuario, String descripcion) {
+		setDescripcion(descripcion);
+		setNombreUsuario(nombreUsuario);
+	}
+	
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
--- src/ar/edu/unlam/pb2/eva2/Persona.java
+++ src/ar/edu/unlam/pb2/eva2/Persona.java
@@ -1,17 +1,18 @@
 package ar.edu.unlam.pb2.eva2;
 
-public class Persona {
+public abstract class Persona {
 	private String nombre;
 	private String apellido;
 	private String password;
-	private String passwordReset;
-	private RolUsuario rol;
+	private Boolean login;
+	protected RolUsuario rol;
 	
 	public Persona(String nombre, String apellido, String password, String passwordReset) {
 		setNombre(nombre);
 		setApellido(apellido);
 		setPassword(password);
-		setPasswordReset(passwordReset);
+		this.login = false;
+		this.rol = RolUsuario.USUARIO;
 	}
 	
 	public String getNombre() {
@@ -29,17 +30,16 @@ public class Persona {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getPassword(String passwordReset) {
-		if(passwordReset.equals(this.passwordReset)) {
-			return this.password;
-		}
-		return null;
-	}
-	public void setPasswordReset(String passwordReset) {
-		this.passwordReset = passwordReset;
+	public String getPassword() {
+		return this.password;
 	}
 	public RolUsuario getRolUsuario() {
 		return this.rol;
 	}
-	
+	public Boolean getLogin() {
+		return this.login;
+	}
+	public void cambiarLogin() {
+		this.login =! this.login;
+	}
 }
